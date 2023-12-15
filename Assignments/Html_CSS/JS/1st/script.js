d6755272883eb777d743c3cc3ba93a6ca7839342
nightcrypt <nightcrypt029@gmail.com>
2023-12-15 18:06:19 +0000

    new code

---
+++ Assignments/Html_CSS/JS/1st/script.js
@@ -0,0 +1,8 @@
+alert("Hello World");
+
+const sum1 = parseInt(prompt("Enter the first number "));
+const sum2 = parseInt(prompt("Enter the second number "));
+
+var sum = sum1+sum2;
+
+document.write(sum);
--- Java/Pattern/SpiralPattern.java
+++ Java/Pattern/SpiralPattern.java
@@ -1,42 +1,47 @@
 package Pattern;
-import java.util.Scanner;
 
-public class SpiralPattern {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int size;
-        System.out.println("\nEnter the number for the matrix you want: ");
-        size = scanner.nextInt();
-        int left = 0, top = size - 1, N = 1;
-        int i, j;
-        int[][] board = new int[size][size];
-
-        for (i = 0; i < size / 2; i++, left++, top--) {
-            // Left to right
-            for (j = left; j <= top; j++, N++) {
-                board[left][j] = N;
-            }
-            // Top to down
-            for (j = left + 1; j <= top; j++, N++) {
-                board[j][top] = N;
-            }
-            // Right to left
-            for (j = top - 1; j >= left; j--, N++) {
-                board[top][j] = N;
-            }
-            // Down to top
-            for (j = top - 1; j >= left + 1; j--, N++) {
-                board[j][left] = N;
-            }
-        }
-
-        System.out.println("~~~~~~~Result~~~~~~~");
-
-        for (i = 0; i < size; i++) {
-            for (j = 0; j < size; j++) {
-                System.out.printf("%-5d", board[i][j]);
-            }
-            System.out.println("\n");
-        }
-    }
+public class spiralPattern  
+{  
+public static void main(String args[])  
+{  
+    int SIZE=10;  
+    int i, j, N;  
+    int[][] board = new int[SIZE][SIZE];  
+    int left, top;  
+    left = 0;  
+    top  = SIZE - 1;  
+    N    = 1;  
+    for(i=1; i<=SIZE/2; i++, left++, top--)  
+    {  
+        //fill from left to right  
+        for(j=left; j<=top; j++, N++)  
+        {  
+            board[left][j] = N;  
+        }  
+        //fill from top to down  
+        for(j=left+1; j<=top; j++, N++)  
+        {  
+            board[j][top] = N;  
+        }  
+        //fill from right to left  
+        for(j=top-1; j>=left; j--, N++)  
+        {  
+            board[top][j] = N;  
+        }  
+        //fill from down to top  
+        for(j=top-1; j>=left+1; j--, N++)  
+        {  
+            board[j][left] = N;  
+        }  
+    }  
+    //print the pattern  
+    for(i=0; i<SIZE; i++)  
+    {  
+        for(j=0; j<SIZE; j++)  
+        {  
+            System.out.printf("%-5d", board[i][j]);  
+        }  
+    System.out.printf("\n");  
+    }  
+}  
 }
